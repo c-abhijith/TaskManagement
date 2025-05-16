eac6366b330bb005c458cf35dd954ff9c27731ca
c-abhijith <cabhijithofficila@gmail.com>
2025-05-16 10:14:11 +0530

    assing is don and integratiom also done requiremets.txt is added

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from taskmanager.views.usermanage_view import login_page,admin_panel,user_panel,create_user_view,custom_logout
+from taskmanager.views.usermanage_view import login_page,admin_panel,user_panel,create_user_view,custom_logout,assign_user,assign_user_create
 
 urlpatterns = [
     path('login/', login_page, name='login_page'),
@@ -8,5 +8,7 @@ urlpatterns = [
     path('userpanel/', user_panel, name='user_panel'),
     path("create/", create_user_view, name="create_user"),
     path('logout/', custom_logout, name='logout'),
+    path('assign-user/', assign_user, name='assign_user'),
+    path("assign/create/", assign_user_create, name="assign_user_create"),
 
 ]
--- taskmanager/views/usermanage_view.py
+++ taskmanager/views/usermanage_view.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from taskmanager.models import Role, CustomUser
+from taskmanager.models import Role, CustomUser,AssignedUser
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import logout
 from django.contrib import messages
+from django.db.models import Count
+
 
 def login_page(request):
     # If user is already logged in
@@ -84,8 +86,51 @@ def create_user_view(request):
 
 
 
-
+@login_required
 def custom_logout(request):
     logout(request)
     messages.success(request, "You have successfully logged out.")
     return redirect('login_page')  # Replace with your login URL name
+
+@login_required
+def assign_user(request):
+    assigned_admins_data = (
+        AssignedUser.objects.values('admin')
+        .annotate(count=Count('user'))
+    )
+    assigned_admins = []
+    for item in assigned_admins_data:
+        try:
+            admin_obj = CustomUser.objects.get(id=item['admin'])
+            assigned_admins.append({
+                'admin': admin_obj,
+                'count': item['count']
+            })
+        except CustomUser.DoesNotExist:
+            continue
+
+    assigned_user_ids = AssignedUser.objects.values_list('user_id', flat=True)
+    unassigned_users = CustomUser.objects.filter(role=Role.USER).exclude(id__in=assigned_user_ids)
+    return render(request, "assign_user.html", {
+        "assigned_admins": assigned_admins,
+        "unassigned_users": unassigned_users
+    })
+
+@login_required
+def assign_user_create(request):
+    admins = CustomUser.objects.filter(role=Role.ADMIN)
+    assigned_user_ids = AssignedUser.objects.values_list('user_id', flat=True)
+    unassigned_users = CustomUser.objects.filter(role=Role.USER).exclude(id__in=assigned_user_ids)
+    if request.method == "POST":
+        admin_id = request.POST.get("admin")
+        user_id = request.POST.get("user")
+
+        if admin_id and user_id:
+            AssignedUser.objects.create(
+                admin_id=admin_id,
+                user_id=user_id
+            )
+            return redirect('assign_user')
+    return render(request, "create_assign_user.html", {
+        "admins": admins,
+        "unassigned_users": unassigned_users})
